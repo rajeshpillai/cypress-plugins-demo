8125d00d1e41c7556923dcff291bc83b2eceb29c
rajesh.algorisys <rajesh.algorisys@ext.idfy.com>
2022-06-03 09:37:34 +0530

    Fix test

--- cypress/e2e/clipboardjs.cy.js
+++ cypress/e2e/clipboardjs.cy.js
@@ -5,16 +5,19 @@ describe("Access clipboard", () => {
   });
 
   it("click to copy", () => {
-    cy.get("button[data-clipboard-action='copy']").realClick();
+    cy.get("button[data-clipboard-action='copy']").click();
   });
 
   it("access the clipboard", async () => {
-    cy.get("button[data-clipboard-action='copy']").realClick();
+    cy.get("button[data-clipboard-action='copy']").click();
 
-    let text =   await cy.task('getClipboard');
-    cy.log("From clipboard: " + text);
-    cy.get("button[data-clipboard-target='#foo']").realClick();
-    const url = await cy.task('getClipboard'); 
-    cy.log("From clipboard: " + url);
+    cy.task('getClipboard').then(text => {
+      cy.log("From clipboard: " + text);
+      cy.get("button[data-clipboard-target='#foo']").click();
+    });
+    
+    cy.task('getClipboard').then(url => {
+      cy.log("After From clipboard: " + url);
+    })
   })
 })
